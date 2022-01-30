3c6ea581763e4d1a2acb7881ebcfaee7fc7279d1
OneYearIsEnough <whb6350060@Gmail.com>
2022-01-30 23:10:00 +0800

    ?

--- 02排序/quick_sort.py
+++ 02排序/quick_sort.py
@@ -15,33 +15,33 @@ import random
 
 def quickSort(nums):
 
-	def _parition(nums, l, r):
-		# 左闭右闭
-		rnd_idx = random.randint(l, r)
-		nums[rnd_idx], nums[l] = nums[l], nums[rnd_idx]
-		e = nums[l]  # 随机找一个元素，放到最左侧
-		i = l
-		for j in range(l + 1, r + 1):
-			if nums[j] < e:
-				nums[i + 1], nums[j] = nums[j], nums[i + 1]
-				i += 1
-		nums[l], nums[i] = nums[i], nums[l]
-		# 此时i左侧的元素全是小于e的
-		return i
+    def _parition(nums, l, r):
+        # 左闭右闭
+        rnd_idx = random.randint(l, r)
+        nums[rnd_idx], nums[l] = nums[l], nums[rnd_idx]
+        e = nums[l]  # 随机找一个元素，放到最左侧
+        i = l
+        for j in range(l + 1, r + 1):
+            if nums[j] < e:
+                nums[i + 1], nums[j] = nums[j], nums[i + 1]
+                i += 1
+        nums[l], nums[i] = nums[i], nums[l]
+        # 此时i左侧的元素全是小于e的
+        return i
 
-	def _quickSort(nums, l, r):
-		if l >= r:
-			return
+    def _quickSort(nums, l, r):
+        if l >= r:
+            return
 
-		partition_idx = _parition(nums, l, r)
-		# 后面排序的时候就不需要在考虑partition_idx位置的元素了
-		_quickSort(nums, l, partition_idx - 1)
-		_quickSort(nums, partition_idx + 1, r)
+        partition_idx = _parition(nums, l, r)
+        # 后面排序的时候就不需要在考虑partition_idx位置的元素了
+        _quickSort(nums, l, partition_idx - 1)
+        _quickSort(nums, partition_idx + 1, r)
 
-	return _quickSort(nums, 0, len(nums) - 1)
+    return _quickSort(nums, 0, len(nums) - 1)
 
 
 if __name__ == "__main__":
-	from utils import checkSort
+    from utils import checkSort
 
-	checkSort(quickSort)
+    checkSort(quickSort)
--- 02排序/quick_sort_threeways.py
+++ 02排序/quick_sort_threeways.py
@@ -12,40 +12,40 @@ import random
 
 def quickSortThreeWays(nums):
 
-	def _quickSortThreeWays(nums, l, r):
-		# 左闭右闭
-		# 把partition函数直接写在_quickSortThreeWays函数里面
-		if l >= r:
-			return
+    def _quickSortThreeWays(nums, l, r):
+        # 左闭右闭
+        # 把partition函数直接写在_quickSortThreeWays函数里面
+        if l >= r:
+            return
 
-		rnd_idx = random.randint(l, r)
-		nums[l], nums[rnd_idx] = nums[rnd_idx], nums[l]
-		e = nums[l]
+        rnd_idx = random.randint(l, r)
+        nums[l], nums[rnd_idx] = nums[rnd_idx], nums[l]
+        e = nums[l]
 
-		lt = l  # 右闭
-		gt = r + 1  # 左闭
-		i = l + 1
+        lt = l  # 右闭
+        gt = r + 1  # 左闭
+        i = l + 1
 
-		while i < gt:
-			if nums[i] < e:
-				nums[lt + 1], nums[i] = nums[i], nums[lt + 1]
-				lt += 1
-				i += 1
-			elif e < nums[i]:
-				nums[i], nums[gt - 1] = nums[gt - 1], nums[i]
-				gt -= 1
-			else:
-				i += 1
+        while i < gt:
+            if nums[i] < e:
+                nums[lt + 1], nums[i] = nums[i], nums[lt + 1]
+                lt += 1
+                i += 1
+            elif e < nums[i]:
+                nums[i], nums[gt - 1] = nums[gt - 1], nums[i]
+                gt -= 1
+            else:
+                i += 1
 
-		nums[l], nums[lt] = nums[lt], nums[l]
-		_quickSortThreeWays(nums, l, lt - 1)
-		_quickSortThreeWays(nums, gt, r)
+        nums[l], nums[lt] = nums[lt], nums[l]
+        _quickSortThreeWays(nums, l, lt - 1)
+        _quickSortThreeWays(nums, gt, r)
 
-	_quickSortThreeWays(nums, 0, len(nums) - 1)
+    _quickSortThreeWays(nums, 0, len(nums) - 1)
 
 
 if __name__ == "__main__":
-	from utils import checkSort
+    from utils import checkSort
 
-	checkSort(quickSortThreeWays)
+    checkSort(quickSortThreeWays)
 
